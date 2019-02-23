bb5cd279261ad49d2a1f41d07fb448df1e896178
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-02-23 11:06:23 +0100

    Add scoreboard

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@ var rockInput = document.getElementById('btn-rock');
 var paperInput = document.getElementById('btn-paper');
 var scissorsInput = document.getElementById('btn-scissors');
 var playerSelection;
+var computerPoints = 0;
+var playerPoints = 0;
 var gameLog = function(text){
     var output = document.getElementById('output');
     output.innerHTML = text;
 };
+var pointsLog = function(computer, player) {
+    var playerScore = document.getElementById('player-score');
+    var computerScore = document.getElementById('computer-score');
+    playerScore.innerHTML = player;
+    computerScore.innerHTML = computer;
+}
 gameLog('Select your move by pushing the apropriate button.');
+pointsLog(computerPoints, playerPoints);
+
 var playerMove = function(playerMove) {
     var computerRandom = Math.floor(Math.random() * 3) + 1;
     console.log(computerRandom);
@@ -29,36 +39,46 @@ var playerMove = function(playerMove) {
     else if(playerMove == 'rock') {
         if(computerMove == 'paper'){
             gameLog('You lose!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Try again.');
+            computerPoints++;
         }
         else {
             gameLog('You win!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Want to go again?');
+            playerPoints++;
         }
     }
     else if(playerMove == 'scissors') {
         if(computerMove == 'paper'){
             gameLog('You win!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Want to go again?');
+            playerPoints++;
         }
         else {
             gameLog('You lose!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Try again.');
+            computerPoints++;
         }
     }
     else if(playerMove == 'paper') {
         if(computerMove == 'scissors') {
             gameLog('You lose!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Try again.');
+            computerPoints++;
         }
         else {
             gameLog('You win!<br>You played ' + playerMove + ' while computer played ' + computerMove + '.<br>Want to go again?');
+            playerPoints++
         }
     }
+    pointsLog(computerPoints, playerPoints);
 };
+
 paperInput.addEventListener('click', function (){
     playerSelection = 'paper';
     playerMove(playerSelection);
 });
+
 rockInput.addEventListener('click', function (){
     playerSelection = 'rock';
     playerMove(playerSelection);
 });
+
 scissorsInput.addEventListener('click', function (){
     playerSelection = 'scissors';
     playerMove(playerSelection);
