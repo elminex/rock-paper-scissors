2772f92e4405bfa2797ab3cef26bf02ca631c979
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-04-02 13:19:55 +0200

    Add modals

--- js/script.js
+++ js/script.js
@@ -1,20 +1,77 @@
-var rockInput = document.getElementById('btn-rock');
-var paperInput = document.getElementById('btn-paper');
-var scissorsInput = document.getElementById('btn-scissors');
 var newGame = document.getElementById('btn-new-game');
-var computerPoints = 0;
-var playerPoints = 0;
-var winPoints;
-var gameReady = 'start';
-var computerMove;
 var winPointsOutput = document.getElementById('win-condition');
 var output = document.getElementById('output');
 var scoreBoard = document.getElementById('game-score');
-
+let tableWrapper = document.getElementById('table-output');
+let params = {
+    round: 0,
+    computerMove: undefined,
+    computerPoints: 0,
+    playerPoints: 0,
+    winPoints: undefined,
+    gameReady: 'start',
+    progress: []
+}
+function createTable() {
+    let table = document.createElement('table');
+    table.setAttribute('id', 'summary-table')
+    tableWrapper.appendChild(table);
+    let tableLabels = ['Round nr', 'Player move', 'AI move', 'Round winner', 'Score'];
+    let tableHeaderRow = document.createElement('tr');
+    table.appendChild(tableHeaderRow);
+    for (let i = 0; i < tableLabels.length; i++) {
+        let tableHeaderData = document.createElement('th');
+        tableHeaderData.innerHTML = tableLabels[i];
+        tableHeaderRow.appendChild(tableHeaderData)
+    }
+    for (let j = 0; j < params.progress.length; j++) {
+        tableRow = document.createElement('tr');
+        for (let key in params.progress[j]) {
+            let tableData = document.createElement('td');
+            tableData.innerHTML = params.progress[j][key];
+            tableRow.appendChild(tableData);
+        }
+        table.appendChild(tableRow);
+    }
+}
 var gameLog = function(text){
     output.innerHTML = text;
 };
-
+let endLog = function(text) {
+    let modal = document.getElementById('game-end');
+    modal.classList.add('show');
+    createTable();
+    let modalOutput = document.getElementById('game-end-output');
+    modalOutput.innerHTML = text;
+    closeModal(modal);
+}
+function closeModal(modal) {
+    window.onclick = function (event) {
+      if (event.target == modal)
+            modal.classList.remove('show');
+    }
+    const close = document.getElementById('close-modal');
+    close.onclick = function () {
+        modal.classList.remove('show');
+    }
+    document.addEventListener('keyup', function(event) {
+      if(event.keyCode === 27) {
+          modal.classList.remove('show');
+      }
+    })
+}
+  
+function tableLog(playerValue, winner) {
+    params.round++;
+    let newObject = {
+        nr: params.round,
+        player: playerValue,
+        computer: params.computerMove,
+        roundWinner: winner,
+        score: params.playerPoints + ' - ' + params.computerPoints,
+    }
+    params.progress.push(newObject);
+}
 var pointsLog = function(player, computer) {
     var score = player + ' - ' + computer;
     scoreBoard.innerHTML = score;
@@ -25,57 +82,61 @@ var winConditionLog = function(text) {
 };
 
 gameLog('Press "New game" to start');
-pointsLog(playerPoints, computerPoints);
+pointsLog(params.playerPoints, params.computerPoints);
 
-paperInput.addEventListener('click', function() {
-    buttonValue('paper');
-});
-rockInput.addEventListener('click', function() {
-    buttonValue('rock');
-});
-scissorsInput.addEventListener('click', function() {
-    buttonValue('scissors');
-});
+moveButtons = document.querySelectorAll('.player-move');
+for (i = 0; moveButtons.length > i; i++) {
+    let moves = moveButtons[i];
+    moves.addEventListener('click', function (e) {
+        let moveValue = e.target.getAttribute('data-move');
+        buttonValue(moveValue)
+    })
+}
 
 var buttonValue = function(selection) {
-    if(gameReady === 'started'){
+    if(params.gameReady === 'started'){
         playerMove(selection);
     }
-    else if(gameReady === 'start') {
+    else if(params.gameReady === 'start'){
         gameLog('Please start the game by clicking the "New game" button');
     }
-    else if(gameReady === 'end') {
+    else if(params.gameReady === 'end'){
         gameLog('Game over, please press the "New game" button to start again.');
     };
 };
 
 var playerMove = function(playerValue) {
     computerRandom();
-    if(playerValue === computerMove) {
+    let winner;
+    if(playerValue === params.computerMove) {
         gameLog('Draw! You both played ' + playerValue + '.<br><br>Next round.<br>Select "Rock", "Papper" or "Sciccors" and press the apropriate button.');
+        winner = 'draw';
     }
-    else if((playerValue === 'rock' && computerMove === 'scissors') || (playerValue === 'scissors' && computerMove === 'paper') || (playerValue === 'paper' && computerMove === "rock")) {
-        playerPoints++;
-        winLoseMsg('win', playerValue, computerMove);
+    else if((playerValue === 'rock' && params.computerMove === 'scissors') || (playerValue === 'scissors' && params.computerMove === 'paper') || (playerValue === 'paper' && params.computerMove === "rock")) {
+        params.playerPoints++;
+        winLoseMsg('win', playerValue, params.computerMove);
+        winner = 'player';
     }
     else{
-        computerPoints++;
-        winLoseMsg('lose', playerValue, computerMove);
+        params.computerPoints++;
+        winLoseMsg('lose', playerValue, params.computerMove);
+        winner = 'AI'
     };
-    pointsLog(playerPoints, computerPoints);
-    scoreCheck(playerPoints, computerPoints);
+    tableLog(playerValue, winner);
+    pointsLog(params.playerPoints, params.computerPoints);
+    scoreCheck(params.playerPoints, params.computerPoints);
 };
 
 var computerRandom = function() {
     var randomNumber = Math.floor(Math.random() * 3) + 1;
     if(randomNumber === 1) {
-        computerMove = 'paper';
+        params.computerMove = 'paper';
     }
     else if(randomNumber === 2) {
-        computerMove = 'rock';
+        params.computerMove = 'rock';
     }
     else {
-        computerMove = 'scissors';
+        params.computerMove = 'scissors';
     };
 };
 
@@ -84,36 +145,43 @@ var winLoseMsg = function(result, playerValue, computerValue) {
 };
 
 var scoreCheck = function(player, computer) {
-    if (player === winPoints) {
-        gameLog('<strong>You win</strong>!<br>You are awesome and beat the super-inteligent AI!<br><br>Press the "New game" button to play again!');
+    if (player === params.winPoints) {
+        gameLog('Press the "New game" button to play again!');
+        endLog('<strong>You win</strong>!<br>You are awesome and beat the super-inteligent AI!<br><br>Close this window and press the "New game" button to play again!');
         endGame();
     }
-    else if(computer === winPoints) { 
-        gameLog('<strong>You lose!</strong><br>The super-inteligent AI has yet again proven to be the best.<br><br>Press the "New game" button to play again!');
+    else if (computer === params.winPoints) { 
+        gameLog('Press the "New game" button to play again!');
+        endLog('<strong>You lose!</strong><br>The super-inteligent AI has yet again proven to be the best.<br><br>Close this window and press the "New game" button to play again!');
         endGame();
     };
 };
 
-var endGame = function() {
-    computerPoints = 0;
-    playerPoints = 0;
+var endGame = function () {
+    params.round = 0;
+    params.computerPoints = 0;
+    params.playerPoints = 0;
     winConditionLog("");
-    winPoints = undefined;
+    params.winPoints = undefined;
     setTimeout(function() {
-        gameReady = 'end';
+        params.gameReady = 'end';
     }, 0);
 };
 
-newGame.addEventListener('click', function(){
-    winPoints = window.prompt('Enter score needed to win');
-    winPoints = Math.abs(parseInt(winPoints, 10));
-    if(typeof winPoints === 'number' && !isNaN(winPoints)) {
-        winConditionLog('Firs to ' + winPoints + ' points wins.');
+newGame.addEventListener('click', function () {
+    if (tableWrapper.hasChildNodes()) {
+        tableWrapper.childNodes[0].remove();
+    }
+    params.progress = [];
+    params.winPoints = window.prompt('Enter score needed to win');
+    params.winPoints = Math.abs(parseInt(params.winPoints, 10));
+    if(typeof params.winPoints === 'number' && !isNaN(params.winPoints)) {
+        winConditionLog('First to ' + params.winPoints + ' points wins.');
         gameLog('Select "Rock", "Papper" or "Sciccors" and press the apropriate button.');
-        computerPoints = 0;
-        playerPoints = 0;
-        pointsLog(playerPoints, computerPoints);
-        gameReady = 'started';
+        params.computerPoints = 0;
+        params.playerPoints = 0;
+        pointsLog(params.playerPoints, params.computerPoints);
+        params.gameReady = 'started';
     }
     else {
         gameLog('Please use a number');
